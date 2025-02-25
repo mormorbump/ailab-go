cb891ef168a2b144bc782e662a7d94f8d908497f
mizchi <miz404@gmail.com>
2025-02-25 13:55:44 +0900

    chore(deno.json): add test configuration to include specific scripts and exclude type-predictor style(tdd-mode.ts): update test for add function to skip execution for future implementation

--- scripts/tdd-mode.ts
+++ scripts/tdd-mode.ts
@@ -1,9 +1,9 @@
+// Impl TODO: later
 declare function add(a: number, b: number): number;
-// declare function
-// Unit Tests
+
 import { expect } from "@std/expect";
 import { test } from "@std/testing/bdd";
 
-test("add", () => {
+test.skip("add", () => {
   expect(add(1, 2)).toBe(3);
 });
