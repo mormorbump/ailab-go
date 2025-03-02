e31a38be10818df060cf73cb96ba4048a1044c8d
mizchi <miz404@gmail.com>
2025-03-02 16:56:06 +0900

    update

---
+++ ddd-sample-light/src/domain/taskRepository.ts
@@ -0,0 +1,18 @@
+/**
+ * タスクリポジトリのインターフェース
+ */
+
+import { Task, TaskFilter, TaskId } from "./types.ts";
+import { Result, ValidationError } from "../core/result.ts";
+
+// タスクリポジトリのインターフェース定義
+export interface TaskRepository {
+  // タスク取得メソッド
+  findById(id: TaskId): Promise<Result<Task | null, Error>>;
+  findAll(): Promise<Result<Task[], Error>>;
+  findByFilter(filter: TaskFilter): Promise<Result<Task[], Error>>;
+
+  // タスク操作メソッド
+  save(task: Task): Promise<Result<void, Error>>;
+  delete(id: TaskId): Promise<Result<void, Error>>;
+}
